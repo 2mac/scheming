e871f788588a533cbd9965abc3b23dc70fa209d9
David McMackins II <contact@mcmackins.org>
2016-01-14 12:27:57 -0600

    Add 2.30

---
+++ chapter2/square-tree.scm
@@ -0,0 +1,20 @@
+;; SICP exercise 2.30
+;; Copyright (C) 2016 David McMackins II
+;;
+;; Copying and distribution of this file, with or without modification,
+;; are permitted in any medium without royalty provided the copyright
+;; notice and this notice are preserved.  This file is offered as-is,
+;; without any warranty.
+
+(define (square-tree tree)
+  (cond ((null? tree) '())
+	((pair? tree) (cons (square-tree (car tree))
+			    (square-tree (cdr tree))))
+	(else (expt tree 2))))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (square-tree-map sub-tree)
+	     (expt sub-tree 2)))
+       tree))
