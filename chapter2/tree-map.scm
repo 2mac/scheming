0d3de8cb393a84c04c14b43fcc08eecb3c510b39
David McMackins II <contact@mcmackins.org>
2016-01-15 11:58:10 -0600

    Add 2.31

---
+++ chapter2/tree-map.scm
@@ -0,0 +1,17 @@
+;; SICP exercise 2.31
+;; Copyright (C) 2016 David McMackins II
+;;
+;; Copying and distribution of this file, with or without modification,
+;; are permitted in any medium without royalty provided the copyright
+;; notice and this notice are preserved.  This file is offered as-is,
+;; without any warranty.
+
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (tree-map proc sub-tree)
+	     (proc sub-tree)))
+       tree))
+
+(define (square-tree tree)
+  (tree-map (lambda (x) (expt x 2)) tree))
